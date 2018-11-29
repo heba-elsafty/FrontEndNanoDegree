5f16126ae4776e76ef37cda5e1cff4c21e0101a0
Heba ELsafty <heba.m.elsafty@gmail.com>
2018-11-29 20:27:41 +0200

    Modfying portfolio image + handling the side links events

--- portfolio/assets/js/main.js
+++ portfolio/assets/js/main.js
@@ -1,4 +1,10 @@
- $('.menu-toggle').click (function(){
-  $(this).toggleClass('open');
-  $(".mobile-menu-hide").toggleClass('open');
+$(function(){
+  $('.menu-toggle').click (function(){
+   $(this).toggleClass('open');
+   $(".mobile-menu-hide").toggleClass('open');
+ });
+
+ $('.main-menu-item').click(function(){
+   $('.menu-toggle').trigger("click");
+ });
 });
